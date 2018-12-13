6391ee9f436959f538e4853af4a77a81a2ae9c19
朴伟雄 <2355468258@qq.com>
2018-12-13 17:17:40 +0800

    chapter-3-flux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-//import ClickCouter from './ClickCouter'
-import * as serviceWorker from './serviceWorker';
-import ControlPanel from "./ControlPanel";
+import ControlPanel from './views/ControlPanel'
 
-//ReactDOM.render(<App />, document.getElementById('root'));
-
-ReactDOM.render(<ControlPanel />, document.getElementById('root'));
-
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+    <ControlPanel/>,
+    document.getElementById('root')
+);
